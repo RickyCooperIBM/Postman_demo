84944f671d80a0b59545a77aa4adb2e89b985551
RickyCooperIBM <ricky.cooper-cic@ibm.com>
2021-09-08 16:12:32 +0100

    Update run-newman.sh

--- newman/run-newman.sh
+++ newman/run-newman.sh
@@ -13,10 +13,8 @@ npm install newman --global --no-spin
 echo "New Newman Version: " `newman --version`
 
 # using the v3 syntax.
-mkdir newman
-newman run $COLLECTION_URL --reporters cli,junit --reporter-junit-export "newman-repo/report.xml" --bail
-ls
-cd newman-repo
-git status
+mkdir testReports 
+newman run $COLLECTION_URL --reporters cli,junit --reporter-junit-export "testReports/report.xml" --bail
+
 
 echo "Complete!"
